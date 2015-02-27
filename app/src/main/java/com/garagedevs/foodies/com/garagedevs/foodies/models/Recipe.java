3584f78ea1c11ab6445680a8d0ffb5b5d5d982b0
swagata-a <swagata.acharyya@gmail.com>
2015-02-27 22:36:08 +0530

    Initial commit

---
+++ app/src/main/java/com/garagedevs/foodies/com/garagedevs/foodies/models/Recipe.java
@@ -0,0 +1,10 @@
+package com.garagedevs.foodies.com.garagedevs.foodies.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by swagataacharyya on 27/02/15.
+ */
+public class Recipe implements Serializable {
+
+}
